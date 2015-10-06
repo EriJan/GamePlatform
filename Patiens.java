7519570928b9f343b735fa9dad05309d8a2012ce
goloconda <flagrans@hotmail.com>
2015-10-06 14:33:28 +0200

    New appearance

--- Patiens.java
+++ Patiens.java
@@ -9,18 +9,34 @@ public class Patiens extends CardGame {
         super();
     }
 
-    Random rand = new Random();
-    int r = rand.nextInt(50);
 
-    DeckHandler myCardDeck = new DeckHandler();
-    CardDeal myCardDeal;
+    DeckHandler patiensCardDeck = new DeckHandler();
+    CardDeal patiensCardDeal;
     CardDeal[] cardDealList = new CardDeal[7];
-    CardDeal[] sortedCardHand = new CardDeal[4];
+    CardDeal[] sortedCardDeal = new CardDeal[4];
     List<PlayingCard> positionDeal1 = new ArrayList<PlayingCard>();
     PatiensPlayingCard pPlayingCard;
+    PatiensPlayingCard[] arrayOfEmptyCards;
+
+
+
+
+
+
+
+
+
+    String[][] cardHolder = {{""},{" __ "}, {"|  |"}, {"|  |"}, {" == "}};
+    String[][] cardHolder2 = {{""},{"   "}, {"   "}, {"   "}, {"   "}};
+    String[] frontNumber = {"  1  ", "  2  ", "  \u2666  ", "   \u2663  ", "   \u2665  ", "  \u2660  "};
+
 
     @Override
     public void runGame() {
+        arrayOfEmptyCards[0] = new PatiensPlayingCard(Suit.Clubs, 0);
+        arrayOfEmptyCards[1] =  new PatiensPlayingCard(Suit.Diamonds, 0);
+        arrayOfEmptyCards[2]= new PatiensPlayingCard(Suit.Hearts, 0);
+        arrayOfEmptyCards[3] = new PatiensPlayingCard(Suit.Spades, 0);
 
         System.out.println("Hej och välkommen till Patiens!");
         setNewPatiensGameDeck();
@@ -30,12 +46,41 @@ public class Patiens extends CardGame {
         printDeal();
 
 
+        for (int i = 0; i < cardDealList.length; i++) {
+            for (int j = cardDealList[i].getDeal().size() - 1; j >= 0; j--) {
+                if (cardDealList[i].getDeal().get(j).getValue() == 1) {
+                    cardDealList[i].getDeal().get(j).getSuit();
+                }
+            }
+        }
     }
 
     public void printDeal() {
 
+
+        for (int i = 0; i < cardHolder.length; i++) {
+            for (int j = 0; j < 1; j++) {
+                if (i == 0) {
+                    for (int k = 0; k < frontNumber.length; k++) {
+                        System.out.print(frontNumber[k]);
+                    }
+                    System.out.print("\n");
+                } else {
+                    System.out.print(cardHolder[i][j] + "  ");
+                    System.out.print(cardHolder2[i][j] + "  ");
+                    System.out.print(cardHolder[i][j] + "  ");
+                    System.out.print(cardHolder[i][j] + "  ");
+                    System.out.print(cardHolder[i][j] + "  ");
+                    System.out.print(cardHolder[i][j] + "  ");
+                    System.out.print("\n");
+                }
+            }
+        }
+        System.out.print("\n");
+
+
         for (int i = 0; i < cardDealList.length; i++) {
-            for (int j = cardDealList[i].getDeal().size()-1; j >= 0; j--) {
+            for (int j = cardDealList[i].getDeal().size() - 1; j >= 0; j--) {
                 if (cardDealList[i].getDeal().get(j).getValue() != 0) {
                     cardDealList[i].getDeal().get(j).turnUp();
                     break;
@@ -44,13 +89,13 @@ public class Patiens extends CardGame {
         }
 
         for (int i = 0; i < 7; i++) {
-            System.out.print(cardDealList[0].getDeal().get(i) + "  ");
-            System.out.print(cardDealList[1].getDeal().get(i) + "  ");
-            System.out.print(cardDealList[2].getDeal().get(i) + "  ");
-            System.out.print(cardDealList[3].getDeal().get(i) + "  ");
-            System.out.print(cardDealList[4].getDeal().get(i) + "  ");
-            System.out.print(cardDealList[5].getDeal().get(i) + "  ");
-            System.out.print(cardDealList[6].getDeal().get(i) + "  ");
+            System.out.print(cardDealList[0].getDeal().get(i) + "   ");
+            System.out.print(cardDealList[1].getDeal().get(i) + "   ");
+            System.out.print(cardDealList[2].getDeal().get(i) + "   ");
+            System.out.print(cardDealList[3].getDeal().get(i) + "   ");
+            System.out.print(cardDealList[4].getDeal().get(i) + "   ");
+            System.out.print(cardDealList[5].getDeal().get(i) + "   ");
+            System.out.print(cardDealList[6].getDeal().get(i) + "   ");
             System.out.print("\n");
         }
 
@@ -62,23 +107,25 @@ public class Patiens extends CardGame {
 
         for (int i = 0; i < cardDealList.length; i++) {
             cardDealList[i] = new CardDeal();
-            cardDealList[i].drawFromDeck(myCardDeck, i + 1);
+            cardDealList[i].drawFromDeck(patiensCardDeck, i + 1);
             int x = 7 - cardDealList[i].getHandSize();
             for (int j = 0; j <= x; j++) {
 
                 cardDealList[i].getDeal().add(new PatiensPlayingCard(Suit.Clubs, 0));
             }
         }
-
-        for (int i = 0; i < 4; i++) {
-            sortedCardHand[i] = new CardDeal();
-            sortedCardHand[i].drawFromDeck(myCardDeck, 0);
-        }
+for (int i = 0; i <4; i++) {
+    for (int j = 0; j < 4; j++) {
+        sortedCardDeal[j] = new CardDeal();
+        sortedCardDeal[j].drawFromDeck(patiensCardDeck, 0);
+        sortedCardDeal[j].getDeal().add(arrayOfEmptyCards[0]);
+    }
+}
     }
 
     public void setNewPatiensGameDeck() {
-        myCardDeck.newDeck();
-        myCardDeck.shuffleDeck();
+        patiensCardDeck.newDeck();
+        patiensCardDeck.shuffleDeck();
     }
 }
 
