58ad5cc8bead2e709025cdb95641b1d99e498dfa
cykeltur <cykeltur@gmail.com>
2015-10-07 16:36:08 +0200

    added a simpel loggo and a gui button.. and run  overloading..

--- CardGameSelector.java
+++ CardGameSelector.java
@@ -29,4 +29,31 @@ public class CardGameSelector {
     }
     //System.exit(0);
   }
+
+
+    public static void run(String Option) {
+        CardGame theGame;
+
+        Scanner userInput = new Scanner(System.in);
+
+        Methods.printSlowly("Lets play !\n");
+
+            String inputStr = Option;
+            if ( inputStr.equals("Quit")) {
+                System.exit(0);
+                System.out.println("Program end.");
+            } else if (inputStr.equals("Blackjack")){
+                theGame = new BlackJack();
+                theGame.runGame();
+            } else if (inputStr.equals("Patiens")){
+                theGame = new Patiens();
+                theGame.runGame();
+            }else if (inputStr.equals("21")) {
+                System.out.println("the game 21");
+                theGame = new the21();
+                theGame.runGame();
+            }
+        }
+        //System.exit(0);
+
 }
